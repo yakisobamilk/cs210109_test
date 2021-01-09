b5689f8a1b0c42db5ec395ef73ee8ed84a6df1f3
yakisobadesk <yhase@ngk.co.jp>
2021-01-09 16:55:29 +0900

    プロジェクト ファイルを追加します。

---
+++ cs210109_test/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs210109_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GitHubに上げてみるだけのサンプル");
+        }
+    }
+}
